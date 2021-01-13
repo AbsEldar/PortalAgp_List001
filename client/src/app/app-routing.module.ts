583841aa051c1f83b6ec92651c56e3a6add0c3cf
Eldar Absalyamov <salyamkin@gmail.com>
2021-01-14 01:38:45 +0600

    end of lesson 116

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { UserDetailsComponent } from './shop/user-details/user-details.component';
@@ -7,6 +8,7 @@ import { UserDetailsComponent } from './shop/user-details/user-details.component
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   // {path: 'shop/:id', component: UserDetailsComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
