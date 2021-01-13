e04a40c069c681f6841b0efc33d3f97a29399647
Eldar Absalyamov <salyamkin@gmail.com>
2021-01-13 12:22:41 +0600

    lessons 107

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, UserItemComponent],
+  declarations: [ShopComponent, UserItemComponent, UserDetailsComponent],
   imports: [
     CommonModule,
     SharedModule
